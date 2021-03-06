2d0c1799c76673417bd188710d5b595acce5940e
seoyoungsoo <iulove37@naver.com>
2021-03-07 01:16:31 +0900

    Refactor: a 태그 위치 변경

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -28,14 +28,14 @@ function LandingPage() {
         var seconds = Math.floor((video.duration - minutes * 60))
 
         return <Col key={index} lg={6} md={8} xs={24}>
-        <a href={`/video/post/${video._id}`}>
             <div style={{ position: 'relative'}}>
-                <img style={{width: '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
-                <div className="duration">
-                    <span>{minutes} : {seconds}</span>
-                </div>
+                <a href={`/video/${video._id}`}>
+                    <img style={{width: '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                    <div className="duration">
+                        <span>{minutes} : {seconds}</span>
+                    </div>
+                </a>
             </div>
-        </a>
         <br />
         <Meta
             avatar={
