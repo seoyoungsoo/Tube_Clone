4d06a4b0bd14423d68e42bd5e3cc17de16536164
seoyoungsoo <iulove37@naver.com>
2021-03-14 23:34:51 +0900

    Feat: 대댓글 기능 추가
    댓글에 이어서 댓글을 달 수 있는 기능 구현

---
+++ client/src/components/views/VideoDetailPage/Sections/SingleComment.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { Comment, Avatar, Button, Input } from 'antd'
+import { useSelector } from 'react-redux'
+import Axios from 'axios'
+
+const { TextArea } = Input
+
+function SingleComment(props) {
+
+  const videoId = props.postId
+  const user = useSelector(state => state.user)
+  const [OpenReply, setOpenReply] = useState(false)
+  const [CommentValue, setCommentValue] = useState("")
+  const onHandleChange = (event) => {
+    setCommentValue(event.currenTarget.CommentValue)
+  }
+
+  const onClickReplyOpen = () => {
+    setOpenReply(!OpenReply)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+
+    const variable = {
+      content: CommentValue,
+      writer: user.userData._id,
+      postId: videoId,
+      responseTo: props.comment._id
+    }
+
+    Axios.post('/api/comment/saveComment', variable)
+      .then(res => {
+        if(res.data.success) {
+          setCommentValue("")
+          props.refreshFunction(res.data.result)
+        } else {
+          alert("댓글 입력에 실패했습니다.")
+        }
+      })
+  }
+
+  const actions = [
+    <span onClick={onClickReplyOpen} key="comment-basic-reply-to">Reply to</span>
+  ]
+
+  return (
+    <div>
+      <Comment 
+        actions={actions}
+        author={props.comment.writer.name}
+        avatar={<Avatar src={props.comment.writer.image} alt />}
+        content={<p>{props.comment.content}</p>}
+      />
+
+      {OpenReply &&
+        <form style={{display: 'flex'}} onSubmit={onSubmit} >
+          <TextArea
+            style={{width: '100%', borderRadius: '5px'}}
+            onChange={onHandleChange}
+            value={CommentValue}
+            placeholder="댓글을 작성해주세요."
+          />
+          <br />
+          <Button style={{width: '20%', height: '52px'}} onClick={onSubmit}>Submit</Button>
+        </form>
+      }
+
+    </div>
+  )
+}
+
+export default SingleComment
